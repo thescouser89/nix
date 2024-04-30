c540aedb5b8c53c0c936e8cd5d6da5a4f4dc78b2
Dustin Kut Moy Cheung <dcheung@redhat.com>
2024-04-30 13:49:54 -0400

    Initial flake config

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # nix
 Trying out nix to replace homebrew
+
+# Initial setup
+1. Git clone this repo
+2. Run: ```nix profile install .``` inside this folder
+3. Run: ```nix profile upgrade --all``` to install the packages
---
+++ flake.nix
@@ -0,0 +1,23 @@
+# from: https://sandstorm.de/de/blog/post/my-first-steps-with-nix-on-mac-osx-as-homebrew-replacement.html
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: {
+    packages."x86_64-linux".default = let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in pkgs.buildEnv {
+      name = "home-packages";
+      paths = with pkgs; [
+        openshift
+        helm
+        kcat
+        k9s
+        git-standup
+        wrk
+      ];
+    };
+  };
+
+}
