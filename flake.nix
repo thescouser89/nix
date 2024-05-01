9519e35911ed190709b9a139438a10882f61b57c
Dustin Kut Moy Cheung <dcheung@redhat.com>
2024-04-30 22:33:00 -0400

    Do not hardcode x86_64-linux

--- flake.nix
+++ flake.nix
@@ -1,25 +1,24 @@
 # from: https://sandstorm.de/de/blog/post/my-first-steps-with-nix-on-mac-osx-as-homebrew-replacement.html
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-  };
-
-  outputs = { self, nixpkgs }: {
-    packages."x86_64-linux".default = let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
-    in pkgs.buildEnv {
-      name = "home-packages";
-      paths = with pkgs; [
-        git-standup
-        k9s
-        kcat
-        kubectl
-        kubectl-tree
-        kubernetes-helm
-        openshift
-        wrk
-      ];
-    };
-  };
-
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+        in
+        {
+            packages.default = pkgs.buildEnv {
+                name = "home-packages";
+                paths = with pkgs; [
+                  git-standup
+                  k9s
+                  kcat
+                  kubectl
+                  kubectl-tree
+                  kubernetes-helm
+                  openshift
+                  wrk
+                ];
+            };
+        }
+    );
 }
