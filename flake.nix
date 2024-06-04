477783b699cd6ba63d5c982259ba611fb8b2ccbf
Dustin Kut Moy Cheung <dcheung@redhat.com>
2024-06-03 22:15:20 -0400

    Add more packages for work

--- flake.nix
+++ flake.nix
@@ -9,11 +9,15 @@
             packages.default = pkgs.buildEnv {
                 name = "home-packages";
                 paths = with pkgs; [
+                  nodePackages.prettier
+                  nodePackages.bower
+                  nodePackages.grunt-cli
                   git-standup           # for work log of commits
                   k9s                   # to see current containers
                   kcat                  # kafka logging
                   kubectl
                   kubernetes-helm
+                  lazygit
                   maestral
                   openshift             # oc-cli
                   quarkus
