1f5a4424cb10b56970e3369d757adaa65e46523a
Dustin Kut Moy Cheung <dcheung@redhat.com>
2025-05-16 23:00:02 -0400

    Updating to new snapshot

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
                   dua
                   lazygit
                   kubetail
-                  lein
+                  leiningen
                   clojure
                 ];
             };
